3a99f21bf995955496b426c67adfe6f62add6556
Gazaunga <Gazaunga@gmail.com>
2018-03-17 22:21:05 -0400

    Update bar.sh

--- bin/bar.sh
+++ bin/bar.sh
@@ -5,7 +5,11 @@ clock() {
   echo -n "$DATETIME"
 }
 
+temp() {
+  sensors|awk 'BEGIN{i=0;t=0;b=0}/id [0-9]/{b=$4};/Core/{++i;t+=$3}END{if(i>0){printf("%0.1f\n",t/i)}else{sub(/[^0-9.]/,"",b);print b}}'
+}
+
 while true; do
-  BAR_INPUT="%{c}$(clock)"
+  BAR_INPUT="%{c}$(clock) : $(temp)"
   echo $BAR_INPUT
 done
